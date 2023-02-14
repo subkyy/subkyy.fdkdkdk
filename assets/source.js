e974a5c4609585cabc03fedb39ad64b4ce420f48
subkyy <subkyykyubii@gmail.com>
2023-02-14 23:57:07 +0800

    meng upload data web ke git hub

---
+++ assets/source.js
@@ -0,0 +1,12 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+function buy(product_name, product_price) {
+  const message = `Halo saya mau beli ${product_name} - ${product_price}`
+  const whatsapp = `https://wa.me/6288806337545?text=${message}`
+  open(whatsapp, "_blank")
+}
+
+function chat() {
+  buy();
+}
+
